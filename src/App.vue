93967f981b51b041e06c39e38e4cccbab54c7b7e
Alex Eremie <146241194+ereit673@users.noreply.github.com>
2025-02-27 19:54:19 +0100

    created first component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <div id="app"></div>
+  
+    <BaseCard></BaseCard>
+  
 </template>
 
 <script>
+import BaseCard from './components/BaseCard.vue';
 
 
 export default {
   name: 'App',
   components: {
-    
+    BaseCard
   }
 }
 </script>
@@ -19,8 +22,13 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
-#app {
+
+body{
   background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
+}
+
+#app {
+  padding: 100px;
   width:100%;
   min-height: 100dvh;
 }
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+    <h1>Test</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore ea, libero, suscipit a nemo aut, facere quaerat
+      iste quis unde accusamus consectetur laboriosam officia dolores quisquam velit quod ex voluptatum!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseCard"
+};
+</script>
+
+<style scoped>
+.card {
+  border-radius: 16px;
+  padding: 20px 32px;
+  gap: 20px;
+  display: flex;
+  flex-direction: column;
+  background-color: rgba(1, 31, 53, 1);
+}
+</style>
